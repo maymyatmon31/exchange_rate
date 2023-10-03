0ab7bbb618a9443379ce4b1597729ff9fefe0b33
maymyatmon31 <mmm.maymyatmon31@gmail.com>
2023-10-04 04:52:54 +0630

    conditional.js

--- js/conditional.js
+++ js/conditional.js
@@ -1,11 +1,37 @@
-function canIcome(wakeUpTime) {
+// function canIcome(wakeUpTime) {
 
+//     let result;
+
+//     if(wakeUpTime < 9) {
+//         return result = "Come In";
+//     }
+//     return result = "Get Out";
+
+// }
+// console.log(canIcome(9));
+// function canImprove(studyTime) {
+//     let result;
+//     if (studyTime > 8) {
+//         return result = "You will improve in your furture";
+//     }
+//     if (studyTime == 8 ) {
+//         return result = "You will improve in your future but you should try more"
+//     } else {
+//         return result = "You will lose your dreams"
+//     }
+// }
+// console.log(canImprove(9));
+// console.log(canImprove(8));
+// console.log(canImprove(4));
+
+function login(correctPassword) {
     let result;
 
-    if(wakeUpTime < 9) {
-        return result = "Come In";
+    if(correctPassword === "mmm1234") {
+        return result = "Successfully login"
     }
-    return result = "Get Out";
-
+    else {
+        return result = "Your password is wrong";
+    }
 }
-console.log(canIcome(9));
+console.log(login("mmm1234"));
